c630f115e6b515b6e62b835126ad4fd8b54a263a
ANavarro96 <alejandronavarro96@hotmail.com>
2024-02-26 11:09:16 +0000

    Commit inicial

---
+++ Assets/Scripts/MovementRootMotion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementRootMotion : MonoBehaviour
+{
+
+    private Animator animatorController;
+
+    void Start()
+    {
+      
+        animatorController = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        // TODO: Establecer animaciones
+
+    }
+
+}
